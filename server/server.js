d294a0ad484e57fcbe2dadf849642f211d9106d4
farhadsarlak <44545547+farhadsarlak@users.noreply.github.com>
2020-03-04 20:07:05 +0330

    user register post method added

--- server/server.js
+++ server/server.js
@@ -2,18 +2,29 @@
 process.env.NODE_CONFIG_DIR = __dirname + '/config';
 
 const config = require('config');
+const express= require('express');
+const _ = require('lodash');
 
 const {User} = require('./model/user');
 
 console.log(`*** LEVEL:${String(config.get('level'))} ***`);
 
+const app = express();
+app.use(express.json());
 
-let newUser= new User({
-      fullname:' فرهاد سرلک',
-      email:'farhad.sarlak64@gmail.com',
-      password:'1234567'
+app.post('/api/users',(req,res)=>{
+      const body = _.pick(req.body,['fullname','email','password']);
+      console.log(body)
+      let user= new User(body);
+
+      user.save().then((user)=>{
+            res.status(200).send(user);
+
+      },err => res.status(400).json({
+            Error: `خطایی رخ داده است ${err}`
+      }))
 });
 
-newUser.save().then((user)=>{
-      console.log("کاربر ایجاد شد",user);
-})
+app.listen(config.get('PORT'),() => {
+      console.log(`Server is running on port ${config.get('PORT')}`);
+});
